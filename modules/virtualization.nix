7b73f354a6c7115541785bd63db67a80587ccab8
bluecxmboo <blu20745@gmail.com>
2025-09-16 14:27:35 +0300

    added two commands to virtualization module

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -3,10 +3,12 @@ options = {
 virtualization.enable = lib.mkEnableOption "enables virtualization";
 };
 config = lib.mkIf config.virtualization.enable {
-  programs.virt-manager.enable = true;
+ programs.virt-manager.enable = true;
 users.users.blue.extraGroups = ["libvirtd"];
 users.groups.libvirtd.members = ["blue"];
 virtualisation.libvirtd.enable = true;
 virtualisation.spiceUSBRedirection.enable = true;
+services.qemuGuest.enable = true;
+services.spice-vdagentd.enable = true;
 };
 }
