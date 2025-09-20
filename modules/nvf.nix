3a6f8ec5096f35fd547c2cf2441475cfea331ed8
bluecxmboo <blu20745@gmail.com>
2025-09-20 17:03:23 +0300

    fixed some matugen stuff

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -6,7 +6,7 @@ name = "catppuccin";
 style = "mocha";
 };
 statusline.lualine.enable = true;
-statusline.lualine.theme = "pywal";
+statusline.lualine.theme = "catppuccin";
 debugger.nvim-dap.enable = true;
 debugger.nvim-dap.ui.enable = true;
 diagnostics.config.underline = true;
