7d8b894a7f8359cec2dcf39cf77bc84ffdc8bb0d
bluecxmboo <blu20745@gmail.com>
2025-09-21 15:49:59 +0300

    added vram optimization for nvidia, thanks to fazzi from hyprland discord

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,7 +1,32 @@
-{pkgs,lib,config,...}: {
+{lib,config,...}: {
 options = { nvidia.enable = lib.mkEnableOption "enables nvidia drivers";
 };
 config = lib.mkIf config.nvidia.enable {
+environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool.json".text = builtins.toJSON {
+        rules =
+          map (proc: {
+            pattern = {
+              feature = "procname";
+              matches = proc;
+            };
+            profile = "No VidMem Reuse";
+          }) [
+            "Hyprland"
+            ".Hyprland-wrapped"
+            "Discord"
+            ".Discord-wrapped"
+            "DiscordCanary"
+            ".DiscordCanary-wrapped"
+            "electron"
+            ".electron-wrapped"
+            "librewolf"
+            ".librewolf-wrapped"
+            ".librewolf-wrapped_"
+            "losslesscut"
+          ];
+      };
+    
+
 hardware.graphics.enable = true;
 services.xserver.videoDrivers = ["nvidia"];
 
@@ -11,6 +36,7 @@ open = true;
 nvidiaSettings = true;
 package = config.boot.kernelPackages.nvidiaPackages.stable;
 };
+ };
 
-};
 }
+
