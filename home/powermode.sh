b279275ed4c78df36b551fdaca285cefbc832f5a
bluecxmboo <blu20745@gmail.com>
2025-09-21 06:38:52 +0300

    added power saver script for hyprland

---
+++ home/powermode.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+echo "::power mode"
+hyprctl reload
+if [[ "$1" = "looks" ]]; then
+#looks
+hyprctl --batch "\
+keyword animations:enabled 1;\
+keyword general:border_size 2;\
+keyword general:gaps_out 13;\
+keyword general:gaps_in 5;\
+keyword decoration:shadow:enabled 1;\
+keyword decoration:blur:enabled 1"
+fi
+
+if [[ "$1" = "power-saver" ]]; then
+#power saver
+hyprctl --batch "\
+keyword animations:enabled 0;\
+keyword general:border_size 1;\
+keyword general:gaps_out 13;\
+keyword general:gaps_in 5;\
+keyword decoration:shadow:enabled 0;\
+keyword decoration:blur:enabled 0;\
+keyword decoration:rounding 0"
+fi
+
+
