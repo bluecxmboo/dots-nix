abf52ba7811f37dc9a7aed69d0e9391ddee08feb
bluecxmboo <blu20745@gmail.com>
2025-09-27 19:30:30 +0300

    changed rofi theme, thanks kianblakley

--- home/bg_select.sh
+++ home/bg_select.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 folder="/home/blue/dots-nix/home/wallpapers/"
-sel=$(rofi -dmenu -theme "fullscreen-preview" < <(for f in "$folder"/*.{jpg,png}; do [ -e "$f" ] && echo -en "$f\0icon\x1f$f\n"; done))
+sel=$(rofi -dmenu -config ~/dots-nix/home/.config/rofi/bgselector.rasi < <(for f in "$folder"/*.{jpg,png}; do [ -e "$f" ] && echo -en "$f\0icon\x1f$f\n"; done))
 /home/blue/dots-nix/home/.local/bin/walset-backend $sel
